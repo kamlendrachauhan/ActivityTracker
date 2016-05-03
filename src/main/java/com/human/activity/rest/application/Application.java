9361c8b4d74ce3125fe1782893fdc41f939a693d
Kamlendra Chauhan <kamlendrachauhan21@gmail.com>
2016-05-03 13:40:16 -0700

    Removed duplicates

--- src/main/java/com/human/activity/rest/application/Application.java
+++ src/main/java/com/human/activity/rest/application/Application.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.human.activity.server.job.PredictWorker;
+import com.human.activity.server.job.RecognizeActivity;
 
 @Configuration
 @ComponentScan({ "com.human.activity.rest", "com.human.activity.server" })
@@ -15,11 +16,21 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
-		// RecognizeActivity.main();
-		// To start the execution of Prediction Activity
-		PredictWorker instance = PredictWorker.getInstance();
-		instance.startWorker();
+		if (args.length > 0) {
+			System.out.println("Choosen Activity Type : "+args[0]);
+			if ("TrainModel".equalsIgnoreCase(args[0])) {
+				RecognizeActivity.main();
+			} else {
+				// To start the execution of Prediction Activity
+				PredictWorker instance = PredictWorker.getInstance();
+				instance.startWorker();
+			}
+		} else {
+			// To start the execution of Prediction Activity
+			PredictWorker instance = PredictWorker.getInstance();
+			instance.startWorker();
 
+		}
 	}
 
 }
